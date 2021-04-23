93cfbbcfa2b205fe6a76b54ea9cad0fe0079cec5
bart holmberg <bart.holmberg@gmail.com>
2021-04-23 14:11:36 -0700

    daily checkin

--- configuration_files/map_builder_k4a.lua
+++ configuration_files/map_builder_k4a.lua
@@ -11,6 +11,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+-- BAH, 4/23
 
 include "pose_graph_k4a.lua"
 
