5b30ab6897cc63b56cd7be9b3aa8adda7ae9a889
bart holmberg <bart.holmberg@gmail.com>
2021-04-29 08:16:53 -0700

    change back to baseline
    change back to baseline

--- configuration_files/map_builder_k4a.lua
+++ configuration_files/map_builder_k4a.lua
@@ -18,7 +18,7 @@ include "pose_graph_k4a.lua"
 MAP_BUILDER = {
   use_trajectory_builder_2d = false,
   use_trajectory_builder_3d = false,
-  num_background_threads = 4,
+  num_background_threads =4,
   pose_graph = POSE_GRAPH,
   collate_by_trajectory = false,
 }
